77cee409a5304ae6df133bedea4633b86f0e5efa
Ilanna Cardoso <91700396+Lanaaa22@users.noreply.github.com>
2025-11-06 12:30:56 -0300

    Delete Delirios-do- directory

--- Delirios-do-/funcoes/imprimir_classificacao.h
+++
@@ -1,9 +0,0 @@
-#ifndef IMPRIMIR_CLASSIFICACAO_H
-#define IMPRIMIR_CLASSIFICACAO_H
-
-#include "../TADs/tad_time.h"
-#include "../TADs/tad_partida.h"
-
-void Classificacao(bdTimes *bdt, bdPartidas *bdp);
-
-#endif
