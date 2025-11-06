5a05813b35285bd23a00f045b9c80052de84a7eb
Ilanna Cardoso <91700396+Lanaaa22@users.noreply.github.com>
2025-11-05 22:34:44 -0300

    Add files via upload

---
+++ Delírios da Ilanninha/funcoes/system.c
@@ -0,0 +1,54 @@
+#include "system.h"
+#include "consultar_time.h"
+#include "consultar_partida.h"
+#include "imprimir_classificacao.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h> //sleep() para colocar um timer
+#include <locale.h>
+
+void lobby(bdTimes *bdt, bdPartidas *bdp) {  
+    setlocale(LC_ALL, "");
+    printf("Escolha 1 opcao:\n\n");
+    printf("1: Consultar Time\n");
+    printf("2: Consultar Partidas\n");
+    printf("3: Imprimir Tabela de Classificação\n");
+    printf("4 - Sair do programa\n");
+    printf("---------------------\n\n");
+    int option;
+    scanf("%d", &option); // lê a opção digitada pelo usuário
+    if (option == 1) {
+        LimpaTela();
+        ConsultarTime(bdt, bdp);
+        Sleep(3000);
+        printf("---------------------\n\n");
+        lobby(bdt,bdp);
+    } 
+    else if (option == 2) {
+        LimpaTela();
+        ConsultarPartida(bdt, bdp);
+        Sleep(3000);
+        printf("---------------------\n\n");
+        lobby(bdt, bdp);
+    } 
+    else if (option == 3) {
+        LimpaTela();
+        Classificacao(bdt, bdp);
+        Sleep(3000);
+        printf("---------------------\n\n");
+        lobby(bdt, bdp);
+    }
+    else if (option == 4) {
+        LimpaTela();
+        printf("Saindo do programa...\n");
+        return;
+    }
+    else {
+        LimpaTela();
+        printf("Digite uma opção válida\n");
+        printf("---------------------\n\n");
+        lobby(bdt, bdp);
+    }
+}
+
