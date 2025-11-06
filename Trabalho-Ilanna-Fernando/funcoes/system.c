1c90d39f71e748deb2f3315e1e3e51de1cfd42da
Ilanna Cardoso <91700396+Lanaaa22@users.noreply.github.com>
2025-11-06 12:34:52 -0300

    Add files via upload

---
+++ Trabalho-Ilanna-Fernando/funcoes/system.c
@@ -0,0 +1,54 @@
+#include "system.h"
+#include "consultar_time.h"
+#include "consultar_partida.h"
+#include "imprimir_classificacao.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <locale.h>
+
+void lobby(bdTimes *bdt, bdPartidas *bdp) {  
+    setlocale(LC_ALL, "");
+    printf("Escolha 1 opcao:\n\n");
+    printf("1: Consultar Time\n");
+    printf("2: Consultar Partidas\n");
+    printf("3: Imprimir Tabela de Classificação\n");
+    printf("4 - Sair do programa\n");
+    printf("---------------------\n\n");
+    char option[5];
+    scanf("%s", &option); // lê a opção digitada pelo usuário
+    if (strcmp(option, "1") == 0) {
+        LimpaTela();
+        ConsultarTime(bdt, bdp);
+        sleep(3);
+        printf("---------------------\n\n");
+        lobby(bdt,bdp);
+    } 
+    else if (strcmp(option, "2") == 0) {
+        LimpaTela();
+        ConsultarPartida(bdt, bdp);
+        sleep(3);
+        printf("---------------------\n\n");
+        lobby(bdt, bdp);
+    } 
+    else if (strcmp(option, "3") == 0) {
+        LimpaTela();
+        Classificacao(bdt, bdp);
+        sleep(3);
+        printf("---------------------\n\n");
+        lobby(bdt, bdp);
+    }
+    else if (strcmp(option, "4") == 0) {
+        LimpaTela();
+        printf("Saindo do programa...\n");
+        return;
+    }
+    else {
+        LimpaTela();
+        printf("Digite uma opção válida\n");
+        printf("---------------------\n\n");
+        lobby(bdt, bdp);
+    }
+}
+
---
+++ Trabalho-Ilanna-Fernando/funcoes/system.h
@@ -0,0 +1,11 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+#include "../TADs/tad_time.h"
+#include "../TADs/tad_partida.h"
+#include "auxiliares.h"
+
+void lobby(bdTimes *bdt, bdPartidas *bdp); // Função para escolha das funcionalidades
+
+
+
+#endif
